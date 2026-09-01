da1f11c0e82bc3336e89c88fa2679023d2dd6a06
alexionutorbu <alexionutorbu@gmail.com>
2026-09-01 12:23:03 +0200

    Gestione disconnessioni lato server

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,2 +1,67 @@
+use std::{
+    error::Error,
+    io::{self, BufReader, BufWriter, Write},
+    net::TcpStream,
+};
+
 //client side
-fn main() {}
+use cards_against_humanity_console::{Message, State};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // lettura info per connettersi al server
+    let stdin = io::stdin();
+    let mut ip_port = String::new();
+    println!("Inserisci IP:Porta del server a cui vuoi connetterti (es. 127.0.0.1:8080)");
+    io::stdout().flush()?;
+    stdin.read_line(&mut ip_port)?;
+    // connessione al server
+    let stream: TcpStream = TcpStream::connect(ip_port.trim())?;
+    println!("Connessione al server avvenuta con successo");
+    /*
+        Questo thread principale sarà sempre in ascolto dei messaggi che il client vorrà inviare al server
+        Tuttavia per notificare il client di cosa dice il server necessitiamo di spawnare un thread che stia
+        constantemente in ascolto sullo stream di lettura
+    */
+    let stream_lettura = stream.try_clone()?; // duplichiamo lo stream, in quanto dobbiamo dare quello di lettura ad un nuovo thread
+    std::thread::spawn(move || {
+        let reader = BufReader::new(stream_lettura); // buffer lettura
+        // dal buffer lettura generiamo un iteratore che interpreta "Messagge"
+        let mut iteratore = serde_json::Deserializer::from_reader(reader).into_iter::<Message>();
+        while let Some(risultato) = iteratore.next() {
+            match risultato {
+                Ok(Message::Info { info }) => {
+                    println!("{info}"); // ogni volta che riceve una info dal server si mettete in attesa
+                }
+                Ok(altro) => {
+                    println!("\nMessaggio ricevuto non gestito a schermo: {:?}", altro);
+                }
+                Err(e) => {
+                    eprintln!("\nConnessione con il server interrotta: {}", e);
+                    break; // Esce dal ciclo se la connessione cade
+                }
+            }
+        }
+    });
+    let mut writer = BufWriter::new(stream);
+    let mut nickname = String::new();
+
+    // Inserimento nickname
+    println!("Inserisci il tuo nickname: ");
+    io::stdout().flush()?;
+    stdin.read_line(&mut nickname)?;
+    nickname = nickname.trim().to_string();
+    // invio messaggio di Connect
+    let msg_connect = Message::Connect { nickname };
+    serde_json::to_writer(&mut writer, &msg_connect)?;
+    writer.flush()?;
+    let mut messaggio = String::new();
+    loop {
+        io::stdout().flush()?;
+        stdin.read_line(&mut messaggio)?;
+        messaggio = messaggio.trim().to_string();
+        if messaggio == "exit" {
+            break;
+        }
+    }
+    Ok(())
+}
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -3,6 +3,7 @@
 use std::collections::HashMap;
 use std::error::Error;
 use std::io::Write;
+use std::os::linux::raw::stat;
 use std::{
     self,
     net::{TcpListener, TcpStream},
@@ -14,6 +15,7 @@ use cards_against_humanity_console::{Message, State};
 #[derive(Clone)]
 struct ServerInfo {
     lista_giocatori: Arc<Mutex<Vec<String>>>,
+    giocatori_online: Arc<Mutex<Vec<String>>>,
     punteggi: Arc<Mutex<HashMap<String, u32>>>,
     stato: Arc<Mutex<State>>,
     mani_giocatori: Arc<Mutex<HashMap<String, Vec<String>>>>,
@@ -23,6 +25,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut is_host = true;
     let info_comuni = ServerInfo {
         lista_giocatori: Arc::new(Mutex::new(Vec::new())),
+        giocatori_online: Arc::new(Mutex::new(Vec::new())),
         punteggi: Arc::new(Mutex::new(HashMap::new())),
         stato: Arc::new(Mutex::new(State::Lobby)),
         mani_giocatori: Arc::new(Mutex::new(HashMap::new())),
@@ -47,7 +50,7 @@ fn gestisci_client(stream: TcpStream, is_host: bool, info: ServerInfo) {
     let mut writer = std::io::BufWriter::new(&stream);
     // 2. Inizializzi l'iteratore JSON
     let mut iteratore = serde_json::Deserializer::from_reader(reader).into_iter::<Message>();
-
+    let mut nickname_salvato: Option<String> = None;
     // 3. Avvii il ciclo di ascolto dei messaggi (il loop che rimarrà attivo)
     while let Some(risultato) = iteratore.next() {
         match risultato {
@@ -58,7 +61,6 @@ fn gestisci_client(stream: TcpStream, is_host: bool, info: ServerInfo) {
                     State::Lobby => {
                         drop(stato_guard); //
                         let mut lista_guard = info.lista_giocatori.lock().unwrap();
-
                         let (nickname_effettivo, modificato) = if lista_guard.contains(&nickname) {
                             println!("Nickname già in uso: {nickname}");
                             (format!("{}{}", nickname, lista_guard.len()), true) // Restituisce il nome modificato
@@ -66,7 +68,7 @@ fn gestisci_client(stream: TcpStream, is_host: bool, info: ServerInfo) {
                             (nickname, false) // Restituisce il nome originale (spostando l'ownership)
                         };
                         lista_guard.push(nickname_effettivo.clone());
-
+                        nickname_salvato=Some(nickname_effettivo.clone());
                         let mut punteggi_guard = info.punteggi.lock().unwrap();
                         punteggi_guard.insert(nickname_effettivo.clone(), 0);
 
@@ -95,17 +97,92 @@ fn gestisci_client(stream: TcpStream, is_host: bool, info: ServerInfo) {
                             eprintln!("Errore nell'invio (flush) del messaggio di Info: {e}");
                         }
                     }
-                    altro => {
-                        println!("Richiesta inviata durante la fase {altro:?}");
+                    _=> {
+                        drop(stato_guard);
+                        let lista_guard = info.lista_giocatori.lock().unwrap();
+                        let mut lista_online_guard = info.giocatori_online.lock().unwrap();
+
+                        if lista_guard.contains(&nickname) && !lista_online_guard.contains(&nickname) {
+                            //Riconnessione valida
+                            lista_online_guard.push(nickname.clone());
+                            
+                            nickname_salvato = Some(nickname.clone());
+
+                            drop(lista_online_guard);
+                            drop(lista_guard);
+
+                            let messaggio_conferma = Message::Info {
+                                info: format!("Bentornato in partita, {nickname}!"),
+                            };
+                            let _ = serde_json::to_writer(&mut writer, &messaggio_conferma);
+                            let _ = writer.flush();
+                        } else {
+                            // Accesso negato
+                            drop(lista_online_guard);
+                            drop(lista_guard);
+
+                            let messaggio_conferma = Message::Info {
+                                info: "Non è possibile connettersi ad una partita già iniziata o riconnettersi usando il nickname di un altro giocatore".to_string(),
+                            };
+                            let _ = serde_json::to_writer(&mut writer, &messaggio_conferma);
+                            let _ = writer.flush();
+                        }
+                        
                     }
                 }
             }
+            Ok(Message::Start)=>{
+                println!("Ricevuto un messaggio start");
+                let stato_guard = info.stato.lock().unwrap(); // accesso allo stato della partita
+                match &*stato_guard {
+                    State::Lobby =>{
+                        drop(stato_guard);
+                        if is_host {
+                            let mut stato =info.stato.lock().unwrap();
+                            *stato=State::Creating;
+                            let messaggio_conferma = Message::Info {
+                                info: "Creazione partita in corso".to_string(),
+                            };
+                            let _ = serde_json::to_writer(&mut writer, &messaggio_conferma);
+                            let _ = writer.flush();
+                            // TODO creazione partita
+                        }else{
+                            let messaggio_conferma = Message::Info {
+                                info: "Non sei l'host".to_string(),
+                            };
+                            let _ = serde_json::to_writer(&mut writer, &messaggio_conferma);
+                            let _ = writer.flush();
+                        }
+                    }
+                    _ =>{}
+                }
+            }
             Ok(altro_messaggio) => {}
-            Err(errore) => {}
+            Err(errore) => {
+                eprintln!("Errore di lettura o disconnessione brusca: {errore}");
+            }
         }
     }
 
-    // Quando il ciclo termina (il client si disconnette), la funzione finisce.
-    // Lo `stream` uscirà dallo scope e verrà rimosso dalla memoria (dropped),
-    // chiudendo automaticamente la connessione TCP [1].
+    /*
+        Quando il ciclo termina (il client si disconnette), la funzione finisce. Tuttavia c'è la possibilità di una disconnessione non voluta
+        Proprio per questo verrà aggiunto alla lista dei giocatori disconnessi
+    */ 
+    if let Some(ref name) = nickname_salvato {
+        let stato_attuale = info.stato.lock().unwrap();
+
+        match &*stato_attuale {
+            State::Lobby=>{
+                // se siamo in lobby il giocatore viene rimosso direttamente
+                println!{"Giocatore {name} rimosso dalla lobby"};
+                info.lista_giocatori.lock().unwrap().retain(|x| x != name);
+                info.punteggi.lock().unwrap().remove(name);
+                info.giocatori_online.lock().unwrap().retain(|x| x != name);
+            }
+            altro=>{
+                println!{"Giocatore {name} disconnesso"};
+                info.giocatori_online.lock().unwrap().retain(|x| x != name);
+            }
+        }
+    }
 }
